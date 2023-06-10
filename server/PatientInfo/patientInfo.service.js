e44035ce6d2a1846da62413e58bd3944c9089fc3
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-06-09 20:17:39 -0500

    add service file

---
+++ server/PatientInfo/patientInfo.service.js
@@ -0,0 +1,64 @@
+
+const knex = require('../config/database');
+
+const getAllUsers = async () => {
+  try {
+    
+    const users = await knex('users').select('*');
+    return users;
+  } catch (error) {
+
+    throw new Error('Failed to fetch users');
+  }
+};
+
+const createUser = async (userData) => {
+  try {
+  
+    const [createdUser] = await knex('users').insert(userData).returning('*');
+    return createdUser;
+  } catch (error) {
+   
+    throw new Error('Failed to create user');
+  }
+};
+
+const getUserById = async (userId) => {
+  try {
+    
+    const user = await knex('users').where('id', userId).first();
+    return user;
+  } catch (error) {
+  
+    throw new Error('Failed to fetch user');
+  }
+};
+
+const updateUser = async (userId, updatedUserData) => {
+  try {
+    
+    const [updatedUser] = await knex('users').where('id', userId).update(updatedUserData).returning('*');
+    return updatedUser;
+  } catch (error) {
+   
+    throw new Error('Failed to update user');
+  }
+};
+
+const deleteUser = async (userId) => {
+  try {
+    
+    await knex('users').where('id', userId).del();
+  } catch (error) {
+    
+    throw new Error('Failed to delete user');
+  }
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
