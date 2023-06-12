2929e661ef8fed2b205fc7afe54bd3640ccd8b3a
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-06-11 22:52:52 -0500

    router file

---
+++ server/PatientInfo/patientInfo.router.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const patientInfoController = require('./patientInfo.controller');
+
+
+router.get('/', patientInfoController.getAllPatientInfo);
+router.post('/', patientInfoController.createPatientInfo);
+router.get('/:id', patientInfoController.getPatientInfoById);
+router.put('/:id', patientInfoController.updatePatientInfo);
+router.delete('/:id', patientInfoController.deletePatientInfo);
+
+module.exports = router;
