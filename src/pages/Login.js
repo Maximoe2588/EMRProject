ebeaaaee62281dd3a686b4f120a0a84fe0f7b0eb
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-06-18 22:52:58 -0500

    add register link

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Login() {
   const navigate = useNavigate();
@@ -33,6 +33,7 @@ function Login() {
         />
         <button type="submit">Login</button>
       </form>
+      <p>Not registered? <Link to="/register">Register here</Link></p>
     </div>
   );
 }
