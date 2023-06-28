ad40d5e05e323929d52d39060a0e0be939c03c52
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-06-28 17:04:57 -0500

    error handling symptoms controller

--- server/SymptomInfo/symptomController.js
+++ server/SymptomInfo/symptomController.js
@@ -1,15 +1,25 @@
 const symptomsService = require('./symptomsService');
 
 const getSymptomsByUserId = async (req, res) => {
+   try{
     const userId = req.params.userId;
     const symptoms = await symptomsService.getSymptomsByUserId(userId);
+    if(!symptoms.length) throw new Error('No symptoms found for this user');
     res.json(symptoms);
+   } catch(err){
+    next(err);
+   }
 };
 
 const createSymptom = async (req, res) => {
+    try{
     const newSymptom = req.body;
     const createdSymptom = await symptomsService.createSymptom(newSymptom);
+    if(!createdSymptom) throw new Error('Could not create symptom');
     res.json(createdSymptom);
+    } catch (err){
+        next(err);
+    }
 };
 
 module.exports = {
