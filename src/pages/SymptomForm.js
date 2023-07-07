698da18bf8d744085bfbe0aeaec31bd977dd94a3
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-07-06 20:27:16 -0500

    update handleSubmit SymptomForm

--- src/pages/SymptomForm.js
+++ src/pages/SymptomForm.js
@@ -19,7 +19,25 @@ function SymptomForm() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle form submission logic here.
+    fetch('/api/symptoms', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   };
 
   return (
