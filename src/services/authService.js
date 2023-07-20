829f8783c8066cf2d76cfa6788e221139b4d872e
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-07-19 20:31:48 -0500

    update authService registration logic

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,7 +6,7 @@ const authService = {
     },
     register: async (name, email, password) => {
         console.log('Registering', name, email, password);
-        const response = await fetch('http://localhost:3000/api/register', {
+        const response = await fetch('http://localhost:3000/register', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
