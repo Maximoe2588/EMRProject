5898d7813f29c6423dff749d7ef542b362ac5b73
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-08-13 15:57:22 -0500

    update authcontroller first last name

--- server/PatientInfo/authController.js
+++ server/PatientInfo/authController.js
@@ -5,16 +5,16 @@ const userService = require('./patientInfo.service');
 const register = async (req, res, next) => {
   console.log("Received registration request for:", req.body);
   try {
-    const { name, email, password } = req.body;
+    const { first_name, last_name, email, password } = req.body;
 
 
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
-    const userData = { name, email, password: hashedPassword };
+    const userData = { first_name, last_name, email, password: hashedPassword };
 
     const createdUser = await userService.createUser(userData);
 
-    res.status(201).json({ message: 'Registration successful', user: { name: createdUser.name, age: createdUser.age } });
+    res.status(201).json({ message: 'Registration successful', user: { first_name: createdUser.first_name, last_name: createdUser.last_name, age: createdUser.age } });
   } catch (error) {
     res.status(500).json({ error: 'Failed to register user' });
     next(error);
