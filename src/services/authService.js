6f6ba6dbff90ae2e66d4d37eb169762c5a4d5fae
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-08-13 15:45:51 -0500

    update first last name authservice

--- src/services/authService.js
+++ src/services/authService.js
@@ -4,14 +4,19 @@ const authService = {
         console.log('Logging in', username, password);
         return Promise.resolve();
     },
-    register: async (name, email, password) => {
-        console.log('Registering', name, email, password);
+    register: async ( firstName, lastName, email, password) => {
+        console.log('Registering', firstName, lastName, email, password);
         const response = await fetch('http://localhost:3000/api/register', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ 
+              first_name: firstName,
+              last_name: lastName,
+              email, 
+              password 
+            }),
           });
           const responseData = await response.json(); 
 
