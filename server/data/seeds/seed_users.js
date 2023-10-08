19b5f8169c9350d0b37f92e3e9975842919e1f7f
Maximoe2588 <70680274+Maximoe2588@users.noreply.github.com>
2023-10-08 12:16:53 -0500

    update seeds

--- server/data/seeds/seed_users.js
+++ server/data/seeds/seed_users.js
@@ -7,16 +7,16 @@
   return knex('users').del()
     .then(function () {
       return knex('users').insert([
-        { name: 'John Doe', email: 'john@example.com' },
-        { name: 'Jane Smith', email: 'jane@example.com' },
-        { name: 'Michael Johnson', email: 'michael@example.com' },
-        { name: 'Emily Brown', email: 'emily@example.com' },
-        { name: 'David Wilson', email: 'david@example.com' },
-        { name: 'Sarah Davis', email: 'sarah@example.com' },
-        { name: 'Christopher Taylor', email: 'christopher@example.com' },
-        { name: 'Olivia Anderson', email: 'olivia@example.com' },
-        { name: 'Daniel Martinez', email: 'daniel@example.com' },
-        { name: 'Sophia Hernandez', email: 'sophia@example.com' }
+        { first_name: 'John', last_name: 'Doe', email: 'john.doe@example.com', password: 'johnsSecurePassword123' },
+        { first_name: 'Jane', last_name: 'Smith', email: 'jane.smith@example.com', password: 'janesSecurePassword123' },
+        { first_name: 'Bob', last_name: 'Brown', email: 'bob.brown@example.com', password: 'bobsSecurePassword123' },
+        { first_name: 'Alice', last_name: 'Johnson', email: 'alice.johnson@example.com', password: 'alicesSecurePassword123' },
+        { first_name: 'Robert', last_name: 'Miller', email: 'robert.miller@example.com', password: 'robertsSecurePassword123' },
+        { first_name: 'Ella', last_name: 'Davis', email: 'ella.davis@example.com', password: 'ellasSecurePassword123' },
+        { first_name: 'Michael', last_name: 'Garcia', email: 'michael.garcia@example.com', password: 'michaelsSecurePassword123' },
+        { first_name: 'Sophia', last_name: 'Rodriguez', email: 'sophia.rodriguez@example.com', password: 'sophiasSecurePassword123' },
+        { first_name: 'William', last_name: 'Martinez', email: 'william.martinez@example.com', password: 'williamsSecurePassword123' },
+        { first_name: 'Olivia', last_name: 'Lopez', email: 'olivia.lopez@example.com', password: 'oliviasSecurePassword123' },
       ]);
     });
 };
